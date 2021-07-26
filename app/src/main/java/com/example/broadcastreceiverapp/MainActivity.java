a70cf3349e42f11d9ad64dbc1ede08787b37fb9f
Kwstas Kaskantiris <kkaskantiris@threenitas.com>
2021-07-26 17:06:46 +0300

    Created the final form of the receiver that prints an message when the intent is broadcasted

--- app/src/main/java/com/example/broadcastreceiverapp/MainActivity.java
+++ app/src/main/java/com/example/broadcastreceiverapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.broadcastreceiverapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        IntentFilter intentFilter = new IntentFilter("switchOnDevice");
+        MyBroadcastReceiver objReceiver = new MyBroadcastReceiver();
+        registerReceiver(objReceiver,intentFilter);
     }
 }
