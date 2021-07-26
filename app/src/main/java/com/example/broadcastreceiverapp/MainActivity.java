77eb2171df1875c94ffb56e63e0e0784fc8ba48e
Kwstas Kaskantiris <kkaskantiris@threenitas.com>
2021-07-26 17:29:07 +0300

    Created the on/off broadcast receiver

--- app/src/main/java/com/example/broadcastreceiverapp/MainActivity.java
+++ app/src/main/java/com/example/broadcastreceiverapp/MainActivity.java
@@ -7,13 +7,19 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private ScreenStateReceiver mReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        IntentFilter intentFilter = new IntentFilter("switchOnDevice");
-        MyBroadcastReceiver objReceiver = new MyBroadcastReceiver();
-        registerReceiver(objReceiver,intentFilter);
+
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+        intentFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        mReceiver = new ScreenStateReceiver();
+        registerReceiver(mReceiver, intentFilter);
+//        IntentFilter intentFilter = new IntentFilter("");
+//        MyBroadcastReceiver objReceiver = new MyBroadcastReceiver();
+//        registerReceiver(objReceiver,intentFilter);
     }
 }
---
+++ app/src/main/java/com/example/broadcastreceiverapp/ScreenStateReceiver.java
@@ -0,0 +1,22 @@
+package com.example.broadcastreceiverapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class ScreenStateReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        if (Intent.ACTION_SCREEN_ON.equals(action)) {
+            Log.i("It is on","It is on");
+            Toast.makeText(context,"Its is on",Toast.LENGTH_LONG).show();
+        } else if (Intent.ACTION_SCREEN_OFF.equals(action)) {
+            Log.i("It is off","It is off");
+            Toast.makeText(context,"Its is off",Toast.LENGTH_LONG).show();
+        }
+    }
+}
